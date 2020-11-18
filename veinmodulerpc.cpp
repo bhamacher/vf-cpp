c0993411141e5cb178b61f19a82913bb22a1f6c3
bhamacher <b.hamacher@zera.de>
2020-11-18 08:03:12 +0100

    Added Indentation to lambda function.
    This is done here for better commit readability.
    
    Signed-off-by: bhamacher <b.hamacher@zera.de>

--- veinmodulerpc.cpp
+++ veinmodulerpc.cpp
@@ -51,54 +51,55 @@ void cVeinModuleRpc::callFunction(const QUuid &p_callId,const QUuid &p_peerId, c
 void cVeinModuleRpc::callFunctionPrivate(const QUuid &p_callId, const QUuid &p_peerId, const QVariantMap &t_rpcParameters)
 {
     const auto rpcHandling = [=]() {
-    QVariantMap returnVal;
-    QVariant fcnRetVal;
+        QVariantMap returnVal;
+        QVariant fcnRetVal;
 
-    // check parameters
-    QStringList requiredParamList = m_parameter.keys();
-    QSet<QString> requiredParamKeys(requiredParamList.begin(), requiredParamList.end());
-    const QVariantMap searchParameters = t_rpcParameters.value(VeinComponent::RemoteProcedureData::s_parameterString).toMap();
-    QStringList searchParamList = searchParameters.keys();
-    requiredParamKeys.subtract(QSet<QString>(searchParamList.begin(), searchParamList.end()));
+        // check parameters
+        QStringList requiredParamList = m_parameter.keys();
+        QSet<QString> requiredParamKeys(requiredParamList.begin(), requiredParamList.end());
+        const QVariantMap searchParameters = t_rpcParameters.value(VeinComponent::RemoteProcedureData::s_parameterString).toMap();
+        QStringList searchParamList = searchParameters.keys();
+        requiredParamKeys.subtract(QSet<QString>(searchParamList.begin(), searchParamList.end()));
 
-    if(requiredParamKeys.isEmpty())
-    {
-        //call actual function
-        bool suc=QMetaObject::invokeMethod(m_object,m_function.toUtf8(),Qt::DirectConnection,
-                                           Q_RETURN_ARG(QVariant, fcnRetVal),
-                                           Q_ARG(QVariantMap,searchParameters)
-                                           );
 
-        // write return value to RemoteProcedureData::Return on success
-        if(suc){
-            returnVal.insert(VeinComponent::RemoteProcedureData::s_resultCodeString, RPCResultCodes::RPC_SUCCESS);
-            returnVal.insert("RemoteProcedureData::Return",fcnRetVal);
+        if(requiredParamKeys.isEmpty())
+        {
+            //call actual function
+            bool suc=QMetaObject::invokeMethod(m_object,m_function.toUtf8(),Qt::DirectConnection,
+                                               Q_RETURN_ARG(QVariant, fcnRetVal),
+                                               Q_ARG(QVariantMap,searchParameters)
+                                               );
+
+            // write return value to RemoteProcedureData::Return on success
+            if(suc){
+                returnVal.insert(VeinComponent::RemoteProcedureData::s_resultCodeString, RPCResultCodes::RPC_SUCCESS);
+                returnVal.insert("RemoteProcedureData::Return",fcnRetVal);
+            }else{
+                returnVal.insert(VeinComponent::RemoteProcedureData::s_resultCodeString, RPCResultCodes::RPC_EINVAL);
+                returnVal.insert(VeinComponent::RemoteProcedureData::s_errorMessageString, QString("Function not implemented"));
+            }
+
+
         }else{
+            // write error msg on error
+            returnVal=t_rpcParameters;
             returnVal.insert(VeinComponent::RemoteProcedureData::s_resultCodeString, RPCResultCodes::RPC_EINVAL);
-            returnVal.insert(VeinComponent::RemoteProcedureData::s_errorMessageString, QString("Function not implemented"));
+            returnVal.insert(VeinComponent::RemoteProcedureData::s_errorMessageString, QString("Missing required parameters: [%1]").arg(requiredParamList.join(',')));
         }
+        // send answer
+        returnVal.insert(VeinComponent::RemoteProcedureData::s_callIdString,t_rpcParameters[VeinComponent::RemoteProcedureData::s_callIdString]);
+        VeinComponent::RemoteProcedureData *resultData = new VeinComponent::RemoteProcedureData();
+        resultData->setEntityId(m_nEntityId);
+        resultData->setEventOrigin(VeinEvent::EventData::EventOrigin::EO_LOCAL);
+        resultData->setEventTarget(VeinEvent::EventData::EventTarget::ET_ALL);
+        resultData->setCommand(VeinComponent::RemoteProcedureData::Command::RPCMD_RESULT);
+        resultData->setProcedureName(m_rpcName);
+        resultData->setInvokationData(returnVal);
 
 
-    }else{
-        // write error msg on error
-        returnVal=t_rpcParameters;
-        returnVal.insert(VeinComponent::RemoteProcedureData::s_resultCodeString, RPCResultCodes::RPC_EINVAL);
-        returnVal.insert(VeinComponent::RemoteProcedureData::s_errorMessageString, QString("Missing required parameters: [%1]").arg(requiredParamList.join(',')));
-    }
-    // send answer
-    returnVal.insert(VeinComponent::RemoteProcedureData::s_callIdString,t_rpcParameters[VeinComponent::RemoteProcedureData::s_callIdString]);
-    VeinComponent::RemoteProcedureData *resultData = new VeinComponent::RemoteProcedureData();
-    resultData->setEntityId(m_nEntityId);
-    resultData->setEventOrigin(VeinEvent::EventData::EventOrigin::EO_LOCAL);
-    resultData->setEventTarget(VeinEvent::EventData::EventTarget::ET_ALL);
-    resultData->setCommand(VeinComponent::RemoteProcedureData::Command::RPCMD_RESULT);
-    resultData->setProcedureName(m_rpcName);
-    resultData->setInvokationData(returnVal);
-
-
-    VeinEvent::CommandEvent *rpcResultEvent = new VeinEvent::CommandEvent(VeinEvent::CommandEvent::EventSubtype::NOTIFICATION, resultData);
-    rpcResultEvent->setPeerId(p_peerId);
-    emit m_pEventSystem->sigSendEvent(rpcResultEvent);
+        VeinEvent::CommandEvent *rpcResultEvent = new VeinEvent::CommandEvent(VeinEvent::CommandEvent::EventSubtype::NOTIFICATION, resultData);
+        rpcResultEvent->setPeerId(p_peerId);
+        emit m_pEventSystem->sigSendEvent(rpcResultEvent);
     };
 
     if(m_threaded){
