28b56968cca55c6c9e8d4f1b88609e8abca73b70
bhamacher <b.hamacher@zera.de>
2021-01-27 14:59:08 +0100

    Added VeinProxyComponent hull.
    VeinProxyComponent will act as proxy for components owned by other entities.
    
    Signed-off-by: bhamacher <b.hamacher@zera.de>

---
+++ src/veinproxycomp.cpp
@@ -0,0 +1,33 @@
+#include "veinproxycomp.h"
+
+using namespace vfCpp;
+
+VeinProxyComp::VeinProxyComp()
+{
+
+}
+
+void VeinProxyComp::setValueByEvent(QVariant value)
+{
+
+}
+
+QVariant VeinProxyComp::getValue()
+{
+
+}
+
+QString VeinProxyComp::getName()
+{
+
+}
+
+void VeinProxyComp::setValue(QVariant value)
+{
+
+}
+
+void VeinProxyComp::setError()
+{
+
+}
---
+++ src/veinproxycomp.h
@@ -0,0 +1,39 @@
+#ifndef VEINPROXYCOMP_H
+#define VEINPROXYCOMP_H
+
+#include <QObject>
+
+#include "veinabstractcomponent.h"
+
+namespace vfCpp {
+
+
+
+class VeinProxyComp : public VeinAbstractComponent
+{
+    Q_OBJECT
+public:
+    explicit VeinProxyComp();
+
+    /**
+     * @brief setValueByEvent
+     * This class is meant for use in generic EventSystem classes and should be used in
+     * processEvent functions.
+     * @param value
+     */
+    void setValueByEvent(QVariant value);
+
+    QVariant getValue();
+    QString getName();
+
+public slots:
+    void setValue(QVariant value); // here we have to emit event for notification
+    void setError(); // here we have to emit event for error notification
+
+signals:
+
+};
+
+}
+
+#endif // VEINPROXYCOMP_H
