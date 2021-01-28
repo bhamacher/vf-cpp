e91b3ca668107ca3e30ee31a45179a06ad176b0f
bhamacher <b.hamacher@zera.de>
2021-01-28 11:24:01 +0100

    Added life to VeinProxyComp
    Fetches data in constructor.
    Sends CCMD_SET Transaction on setValue.
    Emits sigValueChanged on value change.
    
    Signed-off-by: bhamacher <b.hamacher@zera.de>

--- src/veinproxycomp.cpp
+++ src/veinproxycomp.cpp
@@ -1,33 +1,101 @@
 #include "veinproxycomp.h"
 
+#include <vcmp_componentdata.h>
+#include <ve_commandevent.h>
+
 using namespace VfCpp;
 
-VeinProxyComp::VeinProxyComp()
+VeinProxyComp::VeinProxyComp(int p_entityId, VeinEvent::EventSystem *p_eventsystem,int p_subEntId ,QString p_subCompName, QVariant p_initval, TakeOver p_defaultTake)
+    : m_entityId(p_entityId), m_pEventSystem(p_eventsystem), m_subEntityId(p_subEntId), m_subComponentName(p_subCompName), m_value(p_initval), m_defaultTake(p_defaultTake)
 {
-
+    VeinEvent::CommandEvent *cEvent = nullptr;
+    VeinComponent::ComponentData *cData = nullptr;
+    cData = new VeinComponent::ComponentData();
+    cData->setEntityId(p_subEntId);
+    cData->setCommand(VeinComponent::ComponentData::Command::CCMD_FETCH);
+    cData->setEventOrigin(VeinComponent::ComponentData::EventOrigin::EO_LOCAL);
+    cData->setEventTarget(VeinComponent::ComponentData::EventTarget::ET_ALL);
+    cData->setComponentName(p_subCompName);
+    cEvent = new VeinEvent::CommandEvent(VeinEvent::CommandEvent::EventSubtype::TRANSACTION, cData);
+    emit m_pEventSystem->sigSendEvent(cEvent);
 }
 
 void VeinProxyComp::setValueByEvent(QVariant value)
 {
-
+    if(value != getValue()){
+            setValue(value);
+        emit sigValueChanged(value);
+    }
 }
 
 QVariant VeinProxyComp::getValue()
 {
-
+    return m_value;
 }
 
 QString VeinProxyComp::getName()
 {
+    return m_subComponentName;
+}
+
+void VeinProxyComp::setValue(QVariant p_value, TakeOver p_takeOver)
+{
+    switch(p_takeOver){
+    case TakeOver::direct:
+        sendTransaction(VeinComponent::ComponentData::Command::CCMD_SET);
+        m_value=p_value;
+        break;
+    case TakeOver::directWithNotification:
+        sendTransaction(VeinComponent::ComponentData::Command::CCMD_SET);
+        setValueByEvent(p_value);
+        break;
+    case TakeOver::onNotification:
+        sendTransaction(VeinComponent::ComponentData::Command::CCMD_SET);
+        break;
+    default:
+        sendTransaction(VeinComponent::ComponentData::Command::CCMD_SET);
+    }
 
 }
 
-void VeinProxyComp::setValue(QVariant value)
+void VeinProxyComp::setValue(QVariant p_value)
 {
-
+    setValue(p_value,m_defaultTake);
 }
 
 void VeinProxyComp::setError()
 {
 
 }
+
+int VeinProxyComp::getSubEntityId() const
+{
+    return m_subEntityId;
+}
+
+void VeinProxyComp::setSubEntityId(int subEntityId)
+{
+    m_subEntityId = subEntityId;
+}
+
+void VeinProxyComp::sendTransaction(VeinComponent::ComponentData::Command vcmd)
+{
+    VeinComponent::ComponentData *cData;
+
+    cData = new VeinComponent::ComponentData();
+
+    cData->setEntityId(m_subEntityId);
+    cData->setEventOrigin(VeinEvent::EventData::EventOrigin::EO_LOCAL);
+    cData->setEventTarget(VeinEvent::EventData::EventTarget::ET_ALL);
+    cData->setCommand(vcmd);
+    cData->setComponentName(m_subComponentName);
+    cData->setNewValue(m_value);
+
+    VeinEvent::CommandEvent *event;
+    event = new VeinEvent::CommandEvent(VeinEvent::CommandEvent::EventSubtype::TRANSACTION, cData);
+    QUuid id; // null id
+    event->setPeerId(id);
+
+    emit m_pEventSystem->sigSendEvent(event);
+}
+
--- src/veinproxycomp.h
+++ src/veinproxycomp.h
@@ -3,6 +3,9 @@
 
 #include <QObject>
 
+#include <vcmp_componentdata.h>
+#include <ve_eventsystem.h>
+
 #include "veinabstractcomponent.h"
 
 namespace VfCpp {
@@ -13,7 +16,26 @@ class VeinProxyComp : public VeinAbstractComponent
 {
     Q_OBJECT
 public:
-    explicit VeinProxyComp();
+    typedef  QSharedPointer< VeinProxyComp > Ptr;
+    typedef  QWeakPointer< VeinProxyComp > WPtr;
+
+    enum class TakeOver{
+        direct,                     // takes the value but does not create sigValueChanged
+        directWithNotification,     // takes the value and creates sigValueChanged
+        onNotification              // only send the transaction and waits for confirmation
+                                    // Get value will return the old value until the new value is confirmed.
+                                    // That might be never!
+    };
+
+    /**
+     * @brief VeinProxyComp Objects of this class represent components owned by other entities
+     * @param entityId the entite id of the proxy owner
+     * @param eventsystem the eventsystem handling this poxycomponent (veinmoduleentity)
+     * @param subEntName the entitie where the component originates
+     * @param SubCompName the name of the original component
+     * @param initval the proxy component value until the first notification comes in
+     */
+    explicit VeinProxyComp(int p_entityId, VeinEvent::EventSystem *p_eventsystem,int p_subEntName ,QString p_subCompName, QVariant p_initval=QVariant(), TakeOver p_defaultTake=TakeOver::onNotification);
 
     /**
      * @brief setValueByEvent
@@ -23,14 +45,35 @@ public:
      */
     void setValueByEvent(QVariant value);
 
-    QVariant getValue();
-    QString getName();
+    QVariant getValue() override;
+    QString getName() override;
+
+    int getSubEntityId() const;
+    void setSubEntityId(int subEntityId);
 
 public slots:
-    void setValue(QVariant value); // here we have to emit event for notification
+    void setValue(QVariant p_value, TakeOver p_takeOver); // here we have to emit event for notification
+    void setValue(QVariant p_value) override;
     void setError(); // here we have to emit event for error notification
 
 signals:
+
+
+private:
+    int m_entityId;
+    VeinEvent::EventSystem *m_pEventSystem;
+    int m_subEntityId;
+    QString m_subComponentName;
+    QVariant m_value;
+    TakeOver m_defaultTake;
+
+protected:
+    /**
+     * @brief sendNotification
+     * sends data to vein, if something changed
+     * @param vcmd
+     */
+    virtual void sendTransaction(VeinComponent::ComponentData::Command vcmd);
 
 };
 
