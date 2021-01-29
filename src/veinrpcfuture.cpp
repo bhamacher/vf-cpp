b6ea2eca18fe8ec2a1179a6f566739299a49b8ef
bhamacher <b.hamacher@zera.de>
2021-01-29 08:50:13 +0100

    Fill VeinRpcFuture with life.
    An object of this type will be returned, when and rpc
    is called. The object will handle incoming data for the corresponding rpc and
    emit a fitting signal (Finished,Progress,Error).
    
    Signed-off-by: bhamacher <b.hamacher@zera.de>

---
+++ src/veinrpcfuture.cpp
@@ -0,0 +1,85 @@
+#include "veinrpcfuture.h"
+
+#include "vcmp_remoteproceduredata.h"
+
+#include "veinmoduleentity.h"
+
+using namespace VfCpp;
+using namespace VeinComponent;
+
+VeinRpcFuture::VeinRpcFuture(int p_rpcEntityId, QString p_rpcName, QUuid p_rpcUid) :
+    m_rpcEntityId(p_rpcEntityId), m_rpcName(p_rpcName), m_status(RpcStatus::inProgress), m_rpcUniqueId(p_rpcUid)
+
+{
+
+}
+
+QVariant VeinRpcFuture::Return()
+{
+    if(m_resultData.contains("RemoteProcedureData::Return")){
+        return m_resultData.value("RemoteProcedureData::Return");
+    }
+    return QVariant();
+
+}
+
+void VeinRpcFuture::processRpcData(RemoteProcedureData *p_rpcData)
+{
+    QVariantMap invokationData=p_rpcData->invokationData();
+    m_resultData=p_rpcData->invokationData();
+    try {
+        if(!invokationData.contains(RemoteProcedureData::s_callIdString)){
+            throw;
+        }
+        if(invokationData[RemoteProcedureData::s_callIdString] != m_rpcUniqueId){
+            throw;
+        }
+        switch(p_rpcData->command())
+        {
+        case RemoteProcedureData::Command::RPCMD_RESULT:
+        {
+            m_status=RpcStatus::finished;
+            emit sigRPCFinished(VeinRpcFuture::Ptr(this));
+            break;
+        }
+        case RemoteProcedureData::Command::RPCMD_PROGRESS:
+        {
+            m_status=RpcStatus::inProgress;
+            emit sigRPCProgress(VeinRpcFuture::Ptr(this));
+            break;
+        }
+        default:
+            break;
+        }
+    }  catch (...) {
+        m_status=RpcStatus::error;
+        emit sigRPCError(VeinRpcFuture::Ptr(this));
+    }
+
+
+}
+
+QVariantMap VeinRpcFuture::getResultData() const
+{
+    return m_resultData;
+}
+
+int VeinRpcFuture::getRpcEntityId() const
+{
+    return m_rpcEntityId;
+}
+
+QString VeinRpcFuture::getRpcName() const
+{
+    return m_rpcName;
+}
+
+VeinRpcFuture::RpcStatus VeinRpcFuture::getStatus() const
+{
+    return m_status;
+}
+
+QUuid VeinRpcFuture::getRpcUniqueId() const
+{
+    return m_rpcUniqueId;
+}
---
+++ src/veinrpcfuture.h
@@ -0,0 +1,85 @@
+﻿#ifndef VEINRPCFUTURE_H
+#define VEINRPCFUTURE_H
+
+#include <QObject>
+#include <QPointer>
+#include <QVariant>
+#include <QUuid>
+
+#include "vcmp_remoteproceduredata.h"
+
+
+
+namespace VfCpp {
+
+class veinmoduleentity;
+
+
+class VeinRpcFuture : public QObject
+{
+    Q_OBJECT
+public:
+    friend class veinmoduleentity;
+
+
+    typedef QSharedPointer<VeinRpcFuture> Ptr;
+
+    enum class RpcStatus{
+        inProgress,             // rpc is in progress
+        finished,               // rpc in finished and all data are there
+        error                   // some kind of unexpected behaviour
+    };
+
+    explicit VeinRpcFuture(int p_rpcEntityId, QString p_rpcName, QUuid p_rpcUid);
+
+
+    QVariant Return();
+
+    int getRpcEntityId() const;
+
+    QString getRpcName() const;
+
+    RpcStatus getStatus() const;
+
+    QUuid getRpcUniqueId() const;
+
+    QVariantMap getResultData() const;
+
+signals:
+    void sigRPCFinished(VeinRpcFuture::Ptr p_future);
+    void sigRPCProgress(VeinRpcFuture::Ptr p_future);
+    void sigRPCError(VeinRpcFuture::Ptr p_future);
+private:
+    void processRpcData(VeinComponent::RemoteProcedureData *p_rpcData);
+private:
+    /**
+     * @brief m_rpcEntityId
+     * Stores the id of the Entitie where the rpc is located
+     */
+    int m_rpcEntityId;
+    /**
+     * @brief m_rpcName
+     * Stores the rpc name
+     */
+    QString m_rpcName;
+    /**
+     * @brief m_resultData
+     * Stores result or progress data
+     */
+    QVariantMap m_resultData;
+    /**
+     * @brief m_status
+     * Stores rpc status data
+     */
+    RpcStatus m_status;
+    /**
+     * @brief m_RpcUniqueId
+     * unique rpc call id
+     */
+    QUuid m_rpcUniqueId;
+
+};
+
+}
+
+#endif // VEINRPCFUTURE_H
