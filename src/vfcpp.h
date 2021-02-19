d1fd55a043be39e0d00f32b35b386194c618ced2
bhamacher <b.hamacher@zera.de>
2021-02-19 10:18:17 +0100

    Added vfcpp.h.
    vfcpp combines all needed headers to use the full vfcpp api.
    including only vfcpp instead of all the needed headers is much easier.
    
    Signed-off-by: bhamacher <b.hamacher@zera.de>

---
+++ src/vfcpp.h
@@ -0,0 +1,8 @@
+#ifndef VFCPP_H
+#define VFCPP_H
+#include <veinmoduleentity.h>
+#include <veinmodulecomponent.h>
+#include <veinsharedcomp.h>
+#include <veinrpcfuture.h>
+#include <veinlambdavalidator.h>
+#endif // VFCPP_H
